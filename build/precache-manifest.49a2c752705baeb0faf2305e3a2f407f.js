48755f33039f84e31c9d09201e3537cb8e94798c
Andrew428 <avcedm1@gmail.com>
2019-03-06 10:40:57 -0600

    commit

---
+++ build/precache-manifest.49a2c752705baeb0faf2305e3a2f407f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d266365def97e166f4ba",
+    "url": "/static/js/main.d266365d.chunk.js"
+  },
+  {
+    "revision": "d0f8eeea7a4cf1f89f6c",
+    "url": "/static/js/1.d0f8eeea.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9f2d70ff3fcc1b623df9fafbcdd1ea26",
+    "url": "/index.html"
+  }
+];
